2b7f7ca708ba64bd74222f3bd8a2f93438e84c6a
Soham Kulkarni <sohamkrox@gmail.com>
2022-10-24 12:58:34 -0500

    Update index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col items-center py-10 w-screen h-screen p-3 bg-white">
+	<div class="flex flex-row items-center gap-10">
+		<img
+			src="https://opensourceatillinois.com/_next/image?url=%2FOSAI%20tranparent%201.png&w=256&q=75"
+			alt="OSAI logo"
+		/>
+		<div class="flex flex-col gap-5">
+			<h1 class="text-2xl text-gray-500">Open-Source at Illinois</h1>
+			<div class="text-3xl">Svelte & Tailwind Demo</div>
+		</div>
+	</div>
+	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to read the documentation</p>
+</div>
