b36e9f4c49724a7bc38c8d1fde2b1d7bcdb56e31
Soham Kulkarni <sohamkrox@gmail.com>
2022-10-26 21:41:44 -0500

    Add example code

---
+++ src/components/events.svelte
@@ -0,0 +1,23 @@
+<script>
+	let events = [];
+
+	const getEventsFromAPI = async () => {
+		const response = await fetch('https://api.opensourceatillinois.com/events');
+		events = await response.json();
+	};
+
+	getEventsFromAPI();
+</script>
+
+<div class="flex flex-row flex-wrap mx-auto justify-center">
+	{#each events as event}
+		<div class="flex w-96 flex-col items-center bg-gray-300 p-5 m-5 rounded-lg justify-between">
+			<div class="text-lg font-bold text-center">
+				{event.name}
+			</div>
+			<div>
+				{event.when}
+			</div>
+		</div>
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,7 @@
+<script>
+	import EventList from '../components/events.svelte';
+</script>
+
 <div class="flex flex-col items-center py-10 w-screen h-screen p-3 bg-white">
-	<div class="flex flex-row items-center gap-10">
-		<img
-			src="https://opensourceatillinois.com/_next/image?url=%2FOSAI%20tranparent%201.png&w=256&q=75"
-			alt="OSAI logo"
-		/>
-		<div class="flex flex-col gap-5">
-			<h1 class="text-2xl text-gray-500">Open-Source at Illinois</h1>
-			<div class="text-3xl">Svelte & Tailwind Demo</div>
-		</div>
-	</div>
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to read the documentation</p>
+	<EventList />
 </div>
